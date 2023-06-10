8d5e6eb0bebbec901f815c2b49d035ef23e9dea5
carolinesteen <32312963+carolinesteen@users.noreply.github.com>
2023-06-10 12:57:42 +0200

    Creating api call and adding response data to script

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
+  const [weatherData, setWeatherData] = useState({ ready: false});
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      date: "Saturday, 12.52",
+      temperature: response.data.temperature.current,
+      city: response.data.city,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+      description: response.data.condition.description,
+      icon: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+    });
+  }
+  if (weatherData.ready) {
     return (
       <div className="Weather">
         <form>
@@ -22,32 +38,43 @@ export default function Weather() {
             </div>
           </div>
         </form>
-        <h1>Los Angeles</h1>
+        <h1>{weatherData.city}</h1>
         <div className="row">
           <div className="col-6">
             <ul>
-              <li>Thursday, 11 May, 12:13</li>
-              <li>Sunny</li>
+              <li>{weatherData.date}</li>
+              <li className="text-capitalize">{weatherData.description}</li>
               <li>
                 Humidity:{" "}
                 <strong>
-                  <span id="humidity"></span>%
+                  <span id="humidity">{weatherData.humidity}</span>%
                 </strong>
                 , Wind:{" "}
                 <strong>
-                  <span id="wind"></span> km/h
+                  <span id="wind">{weatherData.wind}</span> km/h
                 </strong>
               </li>
             </ul>
           </div>
           <div className="col-6 text-center">
             <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              src={weatherData.icon}
               alt="sunny-icon"
+              className="align-top"
             ></img>
-            <span className="temperature align-middle">20°C</span>
+            <span className="temperature align-top">
+              {Math.round(weatherData.temperature)}
+            </span>
+            <span className="unit align-top">°C</span>
           </div>
         </div>
       </div>
     );
+  } else {
+    let apiKey = "42e4tbb0e36fc43f4faaf7e2bob6c342";
+    let city = "New York";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
