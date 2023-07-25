a7921465e8aa167c99d0875969c5869a7d6c64fa
carolinesteen <32312963+carolinesteen@users.noreply.github.com>
2023-07-25 14:00:46 +1000

    Creating weather forecast

---
+++ src/WeatherForecast.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./WeatherForecast.css";
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    let apiKey = "42e4tbb0e36fc43f4faaf7e2bob6c342";
+    let city = props.city;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
+}
